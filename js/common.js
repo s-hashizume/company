00bb234cb402fd0ad854389b6470ed4659036b75
s-hashizume <s-hashizume@urstudx.com>
2023-07-08 12:58:48 +0900

    font-familyの整理

--- js/common.js
+++ js/common.js
@@ -39,22 +39,23 @@ $(function () {
 
 
 //*---スマホ版のハンバーガーメニュー表示---------
+
 $(window).on('load', function () {
     $('body').removeClass('nav_op');
-  });
-  
-  var state = false;
-  var scrollpos;
-  $('.g_nav_toggle').on('click', function () {
+});
+
+var state = false;
+var scrollpos;
+$('.header-humberger-menu').on('click', function () {
     //    $('body').toggleClass('nav_op');
     // $(this).toggleClass('open');
     if (state == false) {
-      scrollpos = $(window).scrollTop();
-      $('body').addClass('nav_op').css({ top: -scrollpos });
-      state = true;
+        scrollpos = $(window).scrollTop();//*--.scrollTopは現在のウィンドウのスクロール垂直位置を取得するメソッド
+        $('body').addClass('nav_op').css({ top: -scrollpos });
+        state = true;
     } else {
-      $('body').removeClass('nav_op').css({ top: 0 });
-      window.scrollTo(0, scrollpos);
-      state = false;
+        $('body').removeClass('nav_op').css({ top: 0 });
+        window.scrollTo(0, scrollpos);
+        state = false;
     }
-  });
+});
