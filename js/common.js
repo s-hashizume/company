ac7da699a2ca0d3bd7adb05aa2a52dff12e75fba
s-hashizume <s-hashizume@urstudx.com>
2023-07-21 13:51:54 +0900

    top画像の１枚目を表示させる

--- js/common.js
+++ js/common.js
@@ -64,27 +64,52 @@ $('.header-humberger-menu').on('click', function () {
 
 
 //----スライダーに関する設定---------
-const $slide = $(".change-slides")
-  .slick({
-    fade: true,    // fedeオン
-    speed: 1500,   // 画像切り替えにかかる時間（ミリ秒）
-    autoplaySpeed: 3000,   // 自動スライド切り替え速度
-    arrows: false,         // 矢印表示・非表示
-    autoplay: true,        // 自動再生
-    slidesToShow: 1,       // スライド表示数
-    slidesToScroll: 1,     // スライドする数
-    infinite: true         // 無限リピート オン・オフ
-  })
-  .on({
-    beforeChange: function(event, slick, currentSlide, nextSlide) {
-        $(".slick-slide", this).eq(currentSlide).addClass("preve-slide");
-        $(".slick-slide", this).eq(nextSlide).addClass("slide-animation");
-    },
-    afterChange: function() {
-        $(".preve-slide", this).removeClass("preve-slide slide-animation");
-    }
-  });
-$slide.find(".slick-slide").eq(0).addClass("slide-animation");
+// const $slide = $(".change-slides")
+//   .slick({
+//     fade: true,    // fedeオン
+//     speed: 1500,   // 画像切り替えにかかる時間（ミリ秒）
+//     autoplaySpeed: 3000,   // 自動スライド切り替え速度
+//     arrows: false,         // 矢印表示・非表示
+//     autoplay: true,        // 自動再生
+//     slidesToShow: 1,       // スライド表示数
+//     slidesToScroll: 1,     // スライドする数
+//     infinite: true         // 無限リピート オン・オフ
+//   })
+//   .on({
+//     beforeChange: function(event, slick, currentSlide, nextSlide) {
+//         $(".slick-slide", this).eq(currentSlide).addClass("preve-slide");
+//         $(".slick-slide", this).eq(nextSlide).addClass("slide-animation");
+//     },
+//     afterChange: function() {
+//         $(".preve-slide", this).removeClass("preve-slide slide-animation");
+//     }
+//   });
+// $slide.find(".slick-slide").eq(0).addClass("slide-animation");
+
+
+setTimeout(function(){
+    const $slide = $(".change-slides")
+    .slick({
+        fade: true,    // fedeオン
+        speed: 1500,   // 画像切り替えにかかる時間（ミリ秒）
+        autoplaySpeed: 3000,   // 自動スライド切り替え速度
+        arrows: false,         // 矢印表示・非表示
+        autoplay: true,        // 自動再生
+        slidesToShow: 1,       // スライド表示数
+        slidesToScroll: 1,     // スライドする数
+        infinite: true         // 無限リピート オン・オフ
+    })
+    .on({
+        beforeChange: function(event, slick, currentSlide, nextSlide) {
+            $(".slick-slide", this).eq(currentSlide).addClass("preve-slide");
+            $(".slick-slide", this).eq(nextSlide).addClass("slide-animation");
+        },
+        afterChange: function() {
+            $(".preve-slide", this).removeClass("preve-slide slide-animation");
+        }
+    });
+    $slide.find(".slick-slide").eq(0).addClass("slide-animation");
+}, 4500);
 
 
 //--------------------アコーディオンメニューの設定------------------------
