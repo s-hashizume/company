fb92e879930465374e16ed3c67bbad4b1c3f839b
s-hashizume <s-hashizume@urstudx.com>
2023-07-31 11:32:33 +0900

    グローバルメニューをタップしてもリストが展開できない事象を修正

--- js/common.js
+++ js/common.js
@@ -168,7 +168,7 @@ $(window).on('resize', function () {//windowをターゲットとして"resize"�
 
 function headerMenu() {
     const windowWidth = $(window).width();
-    const windowSm = 834;
+    const windowSm = 1367;
     console.log(windowWidth)
     // クリックイベントを無効化
     $('.header-wrapper-menu-button').off('click');
